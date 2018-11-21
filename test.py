02fd045d460704bbe806856dab46739a78eaed34
PetarPeychev <petarpeychev98@gmail.com>
2018-11-21 21:12:32 +0000

    added generation of big images, wrapped them into functions

--- test.py
+++ test.py
@@ -1,20 +1,57 @@
 import eca
 from PIL import Image
+import random
 
-for id in range(255):
-    data = [0 for x in range(199 * 100)]
+def generate_single():
+    for id in range(256):
+        data = [0 for x in range(199 * 100)]
 
-    ec = eca.ECA(id)
+        ec = eca.ECA(id)
 
-    for i in range(100):
-        for j in range(199):
-            data[i * 199 + j] = not ec.array[j]
-        ec.step()
+        for i in range(100):
+            for j in range(199):
+                data[i * 199 + j] = not ec.array[j]
+            ec.step()
 
-    img = Image.new('1', (199, 100))
-    img.putdata(data)
-    img.save('images/eca' + str(id) + '.png')
+        img = Image.new('1', (199, 100))
+        img.putdata(data)
+        img.save('images/eca' + str(id) + '.png')
 
+def generate_random_small():
+    for id in range(256):
+        data = [0 for x in range(199 * 100)]
+
+        ec = eca.ECA(id)
+        ec.array = [random.randint(0, 1) for x in range(199)]
+
+        for i in range(100):
+            for j in range(199):
+                data[i * 199 + j] = not ec.array[j]
+            ec.step()
+
+        img = Image.new('1', (199, 100))
+        img.putdata(data)
+        img.save('images-random/eca' + str(id) + '.png')
+
+def generate_random_big():
+    for id in range(256):
+        data = [0 for x in range(1024 * 512)]
+
+        ec = eca.ECA(id)
+        ec.array = [random.randint(0, 1) for x in range(1024)]
+
+        for i in range(512):
+            for j in range(1024):
+                data[i * 1024 + j] = not ec.array[j]
+            ec.step()
+
+        img = Image.new('1', (1024, 512))
+        img.putdata(data)
+        img.save('images-random-big/eca' + str(id) + '.png')
+
+generate_single()
+generate_random_small()
+generate_random_big()
 
 # while True:
 #     id = int(input('ECM ID: '))
